30da2585492e7d7245b8f4741f01240d803dd99f
purab-git <66621177+purab-git@users.noreply.github.com>
2020-07-13 20:37:42 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+ 
+<pre>
+<strong>Uptime:</strong>
+<?php system("uptime"); ?>
+<br />
+<strong>System Information:</strong>
+<?php system("uname -a"); ?>
+<br />
+<strong>Memory Usage (MB):</strong>
+<?php system("free -m"); ?>
+<br />
+<strong>Disk Usage:</strong>
+<?php system("df -h"); ?>
+<br />
+<strong>CPU Information:</strong>
+<?php system("cat /proc/cpuinfo | grep \"model name\\|processor\""); ?>
+</pre>
